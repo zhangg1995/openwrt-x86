b23276e1588f9dedaec3289bbeca6a2654d732af
shiyu1314 <shiyu1052@gmail.com>
2023-11-27 19:41:45 +0800

    update

--- sh/op2.sh
+++ sh/op2.sh
@@ -5,10 +5,12 @@ sed -i "s/192.168.1.1/192.168.2.2/" package/base-files/files/bin/config_generate
 
 sudo rm -rf package/base-files/files/etc/banner
 
-sed -i "s/%D %V %C/%D $(TZ=UTC-8 date +%Y.%m.%d) by shiyu1314/" package/base-files/files/etc/openwrt_release
+sed -i "s/%D %V %C/%D $(TZ=UTC-8 date +%Y.%m.%d)/" package/base-files/files/etc/openwrt_release
+
+sed -i "s/%R/by shiyu1314/" package/base-files/files/etc/openwrt_release
 
 
-date=$(date +"%Y-%m-%d %H:%M:%S")
+date=$(date +"%Y-%m-%d")
 echo "                                                    " >> package/base-files/files/etc/banner
 echo ".___                               __         .__" >> package/base-files/files/etc/banner
 echo "|   | _____   _____   ____________/  |______  |  |" >> package/base-files/files/etc/banner
@@ -17,6 +19,6 @@ echo "|   |  Y Y  \  Y Y  (  <_> )  | \/|  |  / __ \|  |__" >> package/base-file
 echo "|___|__|_|  /__|_|  /\____/|__|   |__| (____  /____/" >> package/base-files/files/etc/banner
 echo "          \/      \/                        \/      " >> package/base-files/files/etc/banner
 echo " -----------------------------------------------------" >> package/base-files/files/etc/banner
-echo " %D %C ${date} by shiyu1314                     " >> package/base-files/files/etc/banner
+echo "         %D ${date} by shiyu1314                     " >> package/base-files/files/etc/banner
 echo " -----------------------------------------------------" >> package/base-files/files/etc/banner
 echo "                                                      " >> package/base-files/files/etc/banner
