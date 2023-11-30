b87aa4f8cb2dde8bf3d8da9a6e7b65761ef81a88
shiyu1314 <shiyu1052@gmail.com>
2023-11-30 15:12:58 +0800

    update

--- sh/op.sh
+++ sh/op.sh
@@ -4,7 +4,7 @@ shopt -s extglob
 
 svn export https://github.com/shiyu1314/openwrt-onecloud/trunk/target/linux/meson target/linux/meson
 
-svn export https://github.com/immortalwrt/immortalwrt/trunk/package/emortal package/emortal
+svn export https://github.com/shiyu1314/openwrt-onecloud/trunk/default-settings package/default-settings
 
 sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' feeds.conf.default
 
@@ -21,7 +21,5 @@ sed -i "s/192.168.1/192.168.2.2/" package/feeds/kiddin9/base-files/files/bin/con
 sed -i "s/Kiddin'/shiyu1314/" package/feeds/kiddin9/base-files/files/etc/banner
 
 sed -i "s/Kiddin'/shiyu1314/" package/feeds/kiddin9/base-files/files/etc/openwrt_release
-
-sed -i '$a ([ -x /bin/bash ] && ! grep -q "^root.*bash" /etc/passwd) && sed -i "s/^\(root.*\/\)ash/\1bash/g" /etc/passwd' package/emortal/default-settings/files/99-default-settings-chinese
 
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
