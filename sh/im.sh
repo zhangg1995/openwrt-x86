130b2f1a3f6cfd35125a4758d71618f06443fdf3
virkingz <68280845+virkingz@users.noreply.github.com>
2023-12-15 14:16:51 +0800

    Update im.sh

--- sh/im.sh
+++ sh/im.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 echo 'src-git dns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
-./scripts/feeds update -a
-sudo rm -rf feeds/packages/net/mosdns
-sudo rm -rf feeds/luci/applications/luci-app-openclash
-sudo rm -rf package/emortal/default-settings
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
