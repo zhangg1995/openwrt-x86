80976fbb6285293b7c68bb1f62d077e7371d00c3
virkingz <68280845+virkingz@users.noreply.github.com>
2023-12-15 11:55:22 +0800

    Update im.sh

--- sh/im.sh
+++ sh/im.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
-sed -i 's/KERNEL_PATCHVER:=6.2/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 echo 'src-git dns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 ./scripts/feeds update -a
