0ae0c526d93b71e131855a68ad2bec66658402eb
virkingz <68280845+virkingz@users.noreply.github.com>
2023-12-16 14:17:03 +0800

    Update im.sh

--- sh/im.sh
+++ sh/im.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
 echo 'src-git dns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 
 ./scripts/feeds update -a
