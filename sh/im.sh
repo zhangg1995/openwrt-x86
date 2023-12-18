f0c746b9ab551bd4b4fbc4b4d1aaec3751f87c65
virkingz <68280845+virkingz@users.noreply.github.com>
2023-12-18 15:41:18 +0800

    Update im.sh

--- sh/im.sh
+++ sh/im.sh
@@ -4,5 +4,7 @@ svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome pa
 svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
 echo 'src-git dns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 
+sed -i "s/192.168.1.1/192.168.3.254/" package/base-files/files/bin/config_generate
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
