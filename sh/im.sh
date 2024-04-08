901e278a141ff74bf674aedb6a939e140683fb17
virkingz <68280845+virkingz@users.noreply.github.com>
2024-04-08 15:39:56 +0800

    Update im.sh

--- sh/im.sh
+++ sh/im.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/kiddin9/openwrt-packages.git
 cp -rf openwrt-packages/luci-app-openclash package/luci-app-openclash
 cp -rf openwrt-packages/adguardhome package/adguardhome
-cp -rf openwrt-packages/luci-app-openclash package/luci-app-openclash
+cp -rf openwrt-packages/luci-app-adguardhome package/luci-app-adguardhome
 rm -rf openwrt-packages
 echo 'src-git dns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 sed -i "s/192.168.1.1/192.168.3.254/" package/base-files/files/bin/config_generate
