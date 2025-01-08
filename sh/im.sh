d2f5cfc2efeba44429e5d2523a76356a0deb2c92
virkingz <68280845+virkingz@users.noreply.github.com>
2025-01-08 14:39:22 +0800

    Update im.sh

--- sh/im.sh
+++ sh/im.sh
@@ -29,8 +29,5 @@ wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
 wget -qO- $GEO_MMDB_URL > files/etc/openclash/Country.mmdb
 
 chmod +x files/etc/openclash/core/clash*
-#下载ad核心
-#mkdir -p files/usr/bin/AdGuardHome
-#AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_amd64 | awk -F '"' '{print $4}')
-#wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
-#chmod +x files/usr/bin/AdGuardHome/AdGuardHome
+
+sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite --force-depends/' package/Makefile
