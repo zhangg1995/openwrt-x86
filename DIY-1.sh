150eb85ca67ae7cb8ded2a8d38aac2a2d507a4e8
virkingz <68280845+virkingz@users.noreply.github.com>
2025-04-21 21:40:35 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -13,6 +13,9 @@ git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci
 # 4-添加 OpenClash 插件
 sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
+# 5-添加vnt插件
+git clone https://github.com/sirpdboy/luci-app-vnt.git package/vnt
+
 # 5-添加 PassWall 插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
