e490847de63652d9c4e2f90e3a2dd202744d0688
virkingz <68280845+virkingz@users.noreply.github.com>
2025-08-22 15:04:16 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -22,3 +22,7 @@ sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./inc
 
 # 7-只显示CPU型号
 sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
+
+# 8-更新golang
+rm -rf packages/lang/golang
+git clone https://github.com/kenzok8/golang packages/lang/golang
