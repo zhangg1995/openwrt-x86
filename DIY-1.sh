a7d74c3babee06ae8b99e8265a359c48ec467af2
virkingz <68280845+virkingz@users.noreply.github.com>
2025-08-22 15:01:58 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -24,5 +24,4 @@ git clone https://github.com/lmq8267/luci-app-vnt.git package/vnt
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
